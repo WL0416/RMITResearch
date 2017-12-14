634bf369e117e3f4129bc38b02d13e828c51ad04
Wei Li <liwsuper7@gmail.com>
2017-12-14 11:15:29 +1100

    modify directory path format.

--- GrayScale.py
+++ GrayScale.py
@@ -2,14 +2,14 @@ import numpy as np
 import cv2 as cv
 import os
 
-inputpath = ".\\Maps4Test\\"
-outputpath = ".\\grayscale"
+inputpath = "./Maps4Test/"
+outputpath = "./GrayScale/"
 
 # loop the target folder to get the original images' information
-for root,folders,files in os.walk(inputpath):
+for root, folders, files in os.walk(inputpath):
     # loop the images
     for file in files:
-        image = cv.imread(root+"\\"+file)
+        image = cv.imread(root+"/"+file)
         # convert image's color into 8-bit gray
         grayImage = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
@@ -17,6 +17,6 @@ for root,folders,files in os.walk(inputpath):
         cv.imshow(file, image)
 
         # write out the new accessed images
-        cv.imwrite(outputpath+"\\"+file,grayImage)
+        cv.imwrite(outputpath+file, grayImage)
         #cv.waitKey(0)
         cv.destroyAllWindows()
